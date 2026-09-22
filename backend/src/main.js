45bfb5dce3b4784acff331eac31c4aa98374a687
ppguz <ppguzmanmayen@gmail.com>
2026-09-22 09:57:49 -0600

    Fix: little fix

--- backend/src/main.js
+++ backend/src/main.js
@@ -526,8 +526,7 @@ app.delete('/:col', async (req, res) => {
   res.json(result);
 });
 
-// Arranque del servidor
-// Reemplaza tu línea de arranque actual por esta:
+
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`Servidor corriendo en http://127.0.0.1:${PORT}`);
 });
